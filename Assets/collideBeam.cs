7c52d454ddb4ba7fccbc0b69e38d97bd9d4e8a8a
gianluca monti <gm3871@gmail.com>
2018-02-06 19:52:57 +0100

    bagno

---
+++ Assets/collideBeam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collideBeam : MonoBehaviour
+{
+
+    public Color colorStart = Color.red;
+    public Color colorEnd = Color.green;
+    public float duration = 1.0F;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("COLLIDE!!!");
+        
+    }
+}
